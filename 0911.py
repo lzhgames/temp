2f5be1eacf1f208b9f88b86d2867fa23ed556e7f
lzh <6950728@qq.com>
2018-09-11 16:32:45 +0800

    test20180911

---
+++ 0911.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# __author__ = 'Lu'
+import heapq
+print(dict(zip('abcd', range(4))))
+print('ok') if 3>2 else 'ko'
+lista=[1,2,3,4,5]
+print(lista[::2])
+print(lista[1::2])
+
+num2=(3,7,20,90,300)
+print(heapq.nlargest(3,num2))
